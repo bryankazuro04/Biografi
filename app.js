fea320377b3eafdc8101c63881c58a6b61f8d08b
Bryan <mhdalana211201@gmail.com>
2022-06-03 12:12:48 +0700

    menambahkan footer, styling, dan menambahkan animasi

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-gsap.to(".jumbotron h1", {
-  duration: 0.7,
-  text: "Selamat Datang",
-  ease: "none",
-});
+// gsap.to(".jumbotron h1", {
+//   duration: 0.7,
+//   text: "Selamat Datang",
+//   ease: "none",
+// });
+
+// const navItem = document.querySelector('.item-nav')
+
+// navItem.addEventListener('click', () => {
+
+// })
